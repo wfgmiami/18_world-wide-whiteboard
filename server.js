8a0301e690109ffb9a32938d6b439480aca5597b
wfgmiami <wfgmiami@yahoo.com>
2017-03-15 19:08:56 -0400

    first

---
+++ server.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var express = require('express');
+var app = express(); // the app returned by express() is a JavaScript Function. Not something we can pass to our sockets!
+
+// app.listen() returns an http.Server object
+// http://expressjs.com/en/4x/api.html#app.listen
+var server = app.listen(1337, function () {
+    console.log('The server is listening on port 1337!');
+});
+
+app.use(express.static(path.join(__dirname, 'browser')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
